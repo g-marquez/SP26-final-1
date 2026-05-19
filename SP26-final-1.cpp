b2a49aec38f9a0e9e92a90e13d7b902d0dc94700
Gabe Marquez <gabrielmarquez98@gmail.com>
2026-05-19 16:17:07 -0700

    update

--- SP26-final-1.cpp
+++ SP26-final-1.cpp
@@ -11,18 +11,25 @@ const string FILE_NAME = "210-final-1-SP26.txt";
 int main() {
     map<string, int> airports;
 
-    int count = 0;
     string airport;
+    string airport2;
     //code for reading a file taken from lab 38
     ifstream fin(FILE_NAME);
     if (fin.good( )) {
         while (fin >> airport) {
-            
+            airports[airport]++;
+            fin >> airport2;
+            airports[airport2]++;
         }
         fin.close( );
     }
     else {
         cout << "ERROR! Please verify file name/directory and restart program.";
         return 1;
+    }
+
+    cout << "All airport traffic counts:" << endl;
+    for (const auto &pair : airports) {
+        
     }
 }
